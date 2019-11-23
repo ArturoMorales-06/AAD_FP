b09a7a9c1c177bc3afba8fc550f3d68277e31f8e
Arturo Morales <45403642+TheBlackStoneWielder61@users.noreply.github.com>
2019-11-23 17:14:37 -0600

    Add files via upload

--- graph_project.py
+++ graph_project.py
@@ -2,14 +2,27 @@ import networkx as nx
 import matplotlib.pyplot as plt 
 import bfs, sys
 
+def color_graph(G, set):
+    try:
+        node_list = G.nodes()
+        color_map = []
+        for node in node_list:
+            if(set.count(node) == 0):
+                color_map.append('blue')
+            else:
+                color_map.append('red')
+        return color_map
+    except:
+        sys.exit("Invalid Input - CG") 
+
 def khop_neighborhood(G, u, k):
     try:
-        bfs_result = bfs.bfs(G, u, k)
+        bfs_result = bfs.bfs(G, u, k+1)
         neighborhood_list = []
         for i in range(len(bfs_result)):
             neighborhood_list += bfs_result[i]
         khop_neighborhood_graph = nx.subgraph(G, neighborhood_list)
-        nx.draw(khop_neighborhood_graph)
+        nx.draw(khop_neighborhood_graph, with_labels=True)
         plt.show()
     except:
         sys.exit("Invalid Input - KHopN")
@@ -30,7 +43,7 @@ def k_length_path(G, u, k):
                     break
                 else:
                     continue
-        nx.draw(k_path)
+        nx.draw(k_path, with_labels=True)
         plt.show()
     except:
         sys.exit("Invalid Input - KLP")
@@ -39,47 +52,91 @@ def k_independent_set(G, u, k):
     try:
         bfs_result = bfs.bfs(G, u, k)
         bfs_length = len(bfs_result)
-        u_neighbors = list(nx.neighbors(G, u))
-        subgraph_nodes = []
+        adjacent_nodes = []
         independent_set = []
-        subgraph_nodes.append(u)
-        for i in range(1, bfs_length - 1):
-            for node in bfs_result[i]:
-                for neighbor in list(nx.neighbors(G, node)):
-                    if(u_neighbors.count(neighbor) == 0 and neighbor != u and independent_set.count(neighbor) == 0 and subgraph_nodes.count(neighbor) == 0):
-                        independent_set.append(neighbor)
-                        if(subgraph_nodes.count(node) == 0):
-                            subgraph_nodes.append(node)
-                    if(len(independent_set) >= k-1):
+        adj_flag = False
+        for i in range(0,k):
+            if(i % 2 == 0):
+                for node in bfs_result[i]:
+                    current_neighbors = list(nx.neighbors(G, node))
+                    for ind_node in independent_set:
+                        if(current_neighbors.count(ind_node) != 0):
+                            adjacent_nodes.append(node)
+                            adj_flag = True
+                            break
+                        else:
+                            continue
+                    if(adj_flag == False):
+                        independent_set.append(node)
+                    if(len(independent_set) >= k):
                         break
-                    else:
-                        continue
-                if(len(independent_set) >= k-1):
+                    adj_flag = False
+                if(len(independent_set) >= k):
                     break
-            if(len(independent_set) >= k-1):
-                break
-        subgraph_nodes = subgraph_nodes + independent_set
-        k_ind_set_graph = nx.subgraph(G, subgraph_nodes)
-        nx.draw(k_ind_set_graph)
-        plt.show()
+            else:
+                for adj_node in bfs_result[i]:
+                    adjacent_nodes.append(adj_node)
 
+            if(len(independent_set) >= k):
+                break
+        subgraph_nodes = independent_set + adjacent_nodes
+        k_ind_set_graph = nx.subgraph(G, subgraph_nodes)
+        color_map = color_graph(k_ind_set_graph, independent_set)
+        nx.draw(k_ind_set_graph, node_color = color_map, with_labels = True)
+        plt.show()
     except:
         sys.exit("Invalid Input - KIS")
 
+            
+
 # def k_independent_set(G, u, k):
 #     try:
-#         G_nodes = G.nodes()
-#         u_neighbors = list(nx.neighbors(G, u))
-#         subgraph_nodes = []
-#         for i in range(k):
-#             for node in G_nodes:
-#                 if(u_neighbors.count(node) == 0):
-#                     subgraph_nodes.append(node)
+#         bfs_result = bfs.bfs(G, u, k)
+#         bfs_length = len(bfs_result)
+#         neighbors_len = len(bfs_result[1])
+#         subgraph_nodes = [u]
+#         color_map = ['red']
+#         for node in bfs_result[1]:
+#             subgraph_nodes.append(node)
+#             color_map.append('blue')
+#         for i in range(2, bfs_length - 1):
+#             for node in bfs_result[i]:
+#                 if(len(subgraph_nodes) - (neighbors_len + 1) >= k):
 #                     break
 #                 else:
-#                     continue
+#                     subgraph_nodes.append(node)
+#                     color_map.append('red')
+#             if(len(subgraph_nodes) - (neighbors_len + 1) >= k):
+#                 break
+
+#     except:
+
+# def k_independent_set(G, u, k):
+#     try:
+#         bfs_result = bfs.bfs(G, u, k)
+#         bfs_length = len(bfs_result)
+#         u_neighbors = list(nx.neighbors(G, u))
+#         subgraph_nodes = []
+#         independent_set = []
+#         subgraph_nodes.append(u)
+#         for i in range(1, bfs_length - 1):
+#             for node in bfs_result[i]:
+#                 for neighbor in list(nx.neighbors(G, node)):
+#                     if(u_neighbors.count(neighbor) == 0 and neighbor != u and independent_set.count(neighbor) == 0 and subgraph_nodes.count(neighbor) == 0):
+#                         independent_set.append(neighbor)
+#                         if(subgraph_nodes.count(node) == 0):
+#                             subgraph_nodes.append(node)
+#                     if(len(independent_set) >= k-1):
+#                         break
+#                     else:
+#                         continue
+#                 if(len(independent_set) >= k-1):
+#                     break
+#             if(len(independent_set) >= k-1):
+#                 break
+#         subgraph_nodes = subgraph_nodes + independent_set
 #         k_ind_set_graph = nx.subgraph(G, subgraph_nodes)
-#         nx.draw(k_ind_set_graph)
+#         nx.draw(k_ind_set_graph, with_labels=True)
 #         plt.show()
 #     except:
 #         sys.exit("Invalid Input - KIS")
@@ -100,9 +157,9 @@ nx.freeze(tet)
 
 # DEBUG ZONE 
 
-k_independent_set(tet, '1', 5)
+# k_independent_set(tet, '1', 15)
 
-# k_length_path(tet, '1', 4)
+# k_length_path(tet, '1', 10)
 
 # khop_neighborhood(tet, '1', 3)
 
